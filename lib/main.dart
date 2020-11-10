f96acafda2cd90f9b79fe470083f23515474abae
keerthan <chandkeerthan@gmail.com>
2020-11-10 22:46:24 +0530

    BoilerPlate

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp>{
+  String _value = "Hello, World!";
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('my app'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(30.5),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text(_value)
+            ]
+          ),
+        )
+      ),
+    );
+  }
+}
